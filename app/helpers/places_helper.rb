c1db5e5427c93339bc81492e3d8fe9bd49cc502c
Jan Kraus <kraus.jan@gmail.com>
2014-02-28 21:37:02 +0100

    Tweak place descriptions

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,22 @@
 module PlacesHelper
+
+
+  def about_place(place_name)
+
+    dict = [
+      ['Salon fryzjerski','salonie fryzjerskim'],
+      ['Salon Fryzjerski','salonie fryzjerskim'],
+      ['Studio Fryzjerskie', 'studiu fryzjerskim'],
+      ['Studio', 'studiu'],
+      ['Salon', 'salonie'],
+      ['Fryzjer', 'fryzjerze']
+
+    ]
+
+    dict.each { |tr| place_name.gsub!(tr[0], tr[1]) }
+
+    return place_name
+
+  end
+
 end
